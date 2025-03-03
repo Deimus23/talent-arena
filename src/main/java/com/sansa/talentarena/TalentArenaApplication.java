8cd9b38a98c0010b6be5818a1e4e433b7c02734d
Pau Sansa <pausansa59@gmail.com>
2025-03-03 10:24:59 +0100

    first commit

---
+++ src/main/java/com/sansa/talentarena/TalentArenaApplication.java
@@ -0,0 +1,13 @@
+package com.sansa.talentarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentArenaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sansa/talentarena/TalentArenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansa.talentarena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentArenaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
