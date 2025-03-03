8f6d2065c9e53375170af822b86325cd9e009a3d
Antonio Carrasco <carrasco.antonio.233@gmail.com>
2025-03-03 14:11:39 +0100

    dni aurentification

--- src/main/java/com/sansa/talentarena/service/AuthService.java
+++ src/main/java/com/sansa/talentarena/service/AuthService.java
@@ -4,9 +4,12 @@ import com.sansa.talentarena.model.dto.RegisterRequestDTO;
 import com.sansa.talentarena.model.entity.User;
 import com.sansa.talentarena.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -18,6 +21,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JWTService jwtService;
     public String registerUser(RegisterRequestDTO requestDTO) {
+        dniAutentification(requestDTO.getDni());
         User user = User.builder()
                 .id(UUID.randomUUID())
                 .email(requestDTO.getEmail())
@@ -31,5 +35,13 @@ public class AuthService {
 
         User savedUser = userRepository.save(user);
         return jwtService.generateToken((UserDetails) savedUser, new HashMap<>());
+    }
+    public void dniAutentification(String dni){
+        String LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
+        String dniRegex = "^[0-9]{8}[A-HJ-NP-TV-Z]$";
+
+        if (!dni.matches(dniRegex)) {
+          throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
