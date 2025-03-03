70d87d5bc6d22a428bc1f4ca4b2cbd4d90740e1d
Antonio Carrasco <carrasco.antonio.233@gmail.com>
2025-03-03 15:20:49 +0100

    dni aurentification

--- src/main/java/com/sansa/talentarena/controller/AuthController.java
+++ src/main/java/com/sansa/talentarena/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.sansa.talentarena.controller;
 
+import com.sansa.talentarena.model.dto.LoginRequestDTO;
 import com.sansa.talentarena.model.dto.RegisterRequestDTO;
 import com.sansa.talentarena.service.AuthService;
 import lombok.RequiredArgsConstructor;
@@ -17,5 +18,9 @@ public class AuthController {
     @PostMapping("/auth/register")
     public String registerUser(@RequestBody RegisterRequestDTO requestDTO) {
         return this.authService.registerUser(requestDTO);
+    }
+    @PostMapping("/auth/login")
+    public String loginUser(@RequestBody LoginRequestDTO loginRequestDTO){
+        return  this.authService.loginUser(loginRequestDTO);
     }
 }
