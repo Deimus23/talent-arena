b9b57bbee83f008cb2e8601ca33c4e03233cdb95
Pau Sansa <pausansa59@gmail.com>
2025-03-03 18:36:02 +0100

    add token response

--- src/main/java/com/sansa/talentarena/controller/AuthController.java
+++ src/main/java/com/sansa/talentarena/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.sansa.talentarena.controller;
 
+import com.sansa.talentarena.model.dto.AuthSuccessDTO;
 import com.sansa.talentarena.model.dto.LoginRequestDTO;
 import com.sansa.talentarena.model.dto.RegisterRequestDTO;
 import com.sansa.talentarena.service.AuthService;
@@ -14,11 +15,13 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/register")
-    public String registerUser(@RequestBody RegisterRequestDTO requestDTO) {
-        return this.authService.registerUser(requestDTO);
+    public AuthSuccessDTO registerUser(@RequestBody RegisterRequestDTO requestDTO) {
+        String token = this.authService.registerUser(requestDTO);
+        return new AuthSuccessDTO(token);
     }
     @PostMapping("/auth/login")
-    public String loginUser(@RequestBody LoginRequestDTO loginRequestDTO){
-        return  this.authService.loginUser(loginRequestDTO);
+    public AuthSuccessDTO loginUser(@RequestBody LoginRequestDTO loginRequestDTO){
+        String token = this.authService.loginUser(loginRequestDTO);
+        return new AuthSuccessDTO(token);
     }
 }
---
+++ src/main/java/com/sansa/talentarena/model/dto/AuthSuccessDTO.java
@@ -0,0 +1,10 @@
+package com.sansa.talentarena.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthSuccessDTO {
+    private String token;
+}
